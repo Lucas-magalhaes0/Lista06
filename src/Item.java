e8c6689755c426d16ee6f6f5c7f828203c18fac2
Lucas Magalhães <lucas.mcordeiro@outlook.com>
2017-05-08 17:45:30 -0300

    Alteracoes na classe Item

--- src/Item.java
+++ src/Item.java
@@ -2,10 +2,22 @@
 public class Item {
 	String nome;
 	int valCura;
-	boolean isUsed = false;
+	int qtd; //quantidade do tipo de item em questao
 	
-	Item (String nome, int valCura) {
+	Item (String nome, int valCura, int qtd) {
 		this.nome = nome;
 		this.valCura = valCura;
 	}
+	
+	//funcao de uso. Retorna valCura para adicionar ao hp do pokemon, caso qtd > 0
+	public int use() {
+		if (qtd > 0) {
+			qtd--;
+			return valCura;
+		}
+		return 0;
+	}
+	
 }
+
+/* Variaveis: tornar tudo privado depois */
